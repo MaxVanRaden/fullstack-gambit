c181ead4274defd5cbdd6909376328c5542b78d1
Max Van Raden <vanraden@pdx.edu>
2021-05-19 10:53:33 -0700

    Added black piece initialization, finished initialize funcition

--- backend/chess.js
+++ backend/chess.js
@@ -36,21 +36,44 @@ class board {
         }
         for(i = 0; i < 8; ++i) {
             for(k = 0; k < 8; ++i) {
-                if(i == 0) {//rank is 1, white backrank 
-                    if(k == 0 || k == 7) {// A1 and H1, white rooks
+                if(i == 0) { //rank is 1, white backrank 
+                    if(k == 0 || k == 7) { // A1 and H1, white rooks
                         chessboard[i][k].myPiece = piece(true, 5, 'Rook');
                     }
-                    if(k == 1 || k == 6) {// B1 and G1, white knights
+                    if(k == 1 || k == 6) { // B1 and G1, white knights
                         chessboard[i][k].myPiece = piece(true, 3, 'Knight');
                     }
-                    if(k == 2 || k == 5) {// C1 and F1, white bishops
+                    if(k == 2 || k == 5) { // C1 and F1, white bishops
                         chessboard[i][k].myPiece = piece(true, 3, 'Bishop');
                     }
-                    if(k == 3) {// D1, white queen
+                    if(k == 3) { // D1, white queen
                         chessboard[i][k].myPiece = piece(true, 9, 'Queen');
                     }
-                    if(k == 4) {// E1, white king
+                    if(k == 4) { // E1, white king
                         chessboard[i][k].myPiece = piece(true, Infinity, 'King');
+                    }
+                }
+                if(i == 1) { //Rank 2, white pawns 
+                    chessboard[i][k].myPiece = piece(true, 1, 'Pawn');
+                }
+                if(i == 6) { //Rank 7, black pawns
+                    chessboard[i][k].myPiece = piece(false, 1, 'Pawn');
+                }
+                if(i == 7) { //rank is 8, black backrank 
+                    if(k == 0 || k == 7) { // A8 and H8, black rooks
+                        chessboard[i][k].myPiece = piece(false, 5, 'Rook');
+                    }
+                    if(k == 1 || k == 6) { // B8 and G8, black knights
+                        chessboard[i][k].myPiece = piece(false, 3, 'Knight');
+                    }
+                    if(k == 2 || k == 5) { // C8 and F8, black bishops
+                        chessboard[i][k].myPiece = piece(false, 3, 'Bishop');
+                    }
+                    if(k == 3) { // D8, black queen
+                        chessboard[i][k].myPiece = piece(false, 9, 'Queen');
+                    }
+                    if(k == 4) { // E8, black king
+                        chessboard[i][k].myPiece = piece(false, Infinity, 'King');
                     }
                 }
             }
